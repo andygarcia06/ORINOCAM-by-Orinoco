4329b69aa56a37db4925a6198c40ce0d8b1ef2d7
Andy Garcia <andygarcia@live.fr>
2021-03-07 21:55:34 +0100

    ajout de la fonction secondTitle à l'appel

--- js/index.js
+++ js/index.js
@@ -110,6 +110,7 @@ promiseGet()
         for(let i = 0; i < response.length; i++) {
             const newSection = document.createElement("section");
             catalogue.appendChild(newSection);
+            sectionTitles();
             insertImage(newSection, response[i].imageUrl);
             const newDiv1 = document.createElement("div");
             newSection.appendChild(newDiv1);
